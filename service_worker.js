98ab0a1dc8e70995f2e96ec4ac0208eb5cf2d396
junglesub <junglesubmarine@gmail.com>
2025-06-07 18:14:52 +0900

    Add whitelist domain check and update permission settings in service worker

--- service_worker.js
+++ service_worker.js
@@ -1,3 +1,12 @@
+// 화이트리스트 도메인 배열
+const whitelist = ["https://github.com"];
+
 chrome.tabs.onZoomChange.addListener((zoomChangeInfo) => {
-	chrome.tabs.setZoomSettings(zoomChangeInfo.tabId, { scope: "per-tab" });
+  chrome.tabs.get(zoomChangeInfo.tabId, (tab) => {
+    if (!tab || !tab.url) return;
+    // whitelist에 포함된 도메인으로 시작하는지 확인
+    if (whitelist.some((domain) => tab.url.startsWith(domain))) {
+      chrome.tabs.setZoomSettings(zoomChangeInfo.tabId, { scope: "per-tab" });
+    }
+  });
 });
